747f3e85004d818b5a73e0c1e77cc4e4e90764e0
Deepak Sattiraju <ds-ms@github.com>
2021-02-17 23:41:57 +0530

    Auth module

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,84 @@
+
+import * as core from '@actions/core';
+// Replace @ds-ms/actions-utility with @azure-actions/utilities once published
+import { WebRequest, WebRequestOptions, WebResponse, sendRequest } from "@ds-ms/actions-utilities/lib/http";
+import { IExecSyncResult, execSync, IExecSyncOptions } from "@ds-ms/actions-utilities";
+import * as querystring from 'querystring';
+
+export async function getAzureAccessToken(creds?: string): Promise<string> {
+    if (!!creds) {
+        return getTokenUsingCredObject(creds);
+    }
+    else {
+        try {
+            let token = getTokenUsingAzCLI();
+            return Promise.resolve(token);
+        } catch (ex) {
+            return Promise.reject(ex);
+        }
+    }
+}
+
+async function getTokenUsingCredObject(creds: string) {
+    let credsObject: { [key: string]: string; };
+    try {
+        credsObject = JSON.parse(creds);
+    } catch (ex) {
+        throw new Error('Credentials object is not a valid JSON');
+    }
+
+    let servicePrincipalId: string = credsObject["clientId"];
+    let servicePrincipalKey: string = credsObject["clientSecret"];
+    let tenantId: string = credsObject["tenantId"];
+    let authorityUrl: string = credsObject["activeDirectoryEndpointUrl"] || "https://login.microsoftonline.com";
+    let managementEndpointUrl: string = credsObject["resourceManagerEndpointUrl"] || "https://management.azure.com/";
+    if (!servicePrincipalId || !servicePrincipalKey || !tenantId || !authorityUrl) {
+        throw new Error("Not all values are present in the creds object. Ensure appId, password and tenant are supplied");
+    }
+    return new Promise<string>((resolve, reject) => {
+        let webRequest = new WebRequest();
+        webRequest.method = "POST";
+        webRequest.uri = `${authorityUrl}/${tenantId}/oauth2/token/`;
+        webRequest.body = querystring.stringify({
+            resource: managementEndpointUrl,
+            client_id: servicePrincipalId,
+            grant_type: "client_credentials",
+            client_secret: servicePrincipalKey
+        });
+        webRequest.headers = {
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+        };
+
+        let webRequestOptions: WebRequestOptions = {
+            retriableStatusCodes: [400, 408, 409, 500, 502, 503, 504],
+        };
+
+        sendRequest(webRequest, webRequestOptions).then(
+            (response: WebResponse) => {
+                if (response.statusCode == 200) {
+                    resolve(response.body.access_token);
+                }
+                else if ([400, 401, 403].indexOf(response.statusCode) != -1) {
+                    reject('ExpiredServicePrincipal');
+                }
+                else {
+                    reject('CouldNotFetchAccessTokenforAzureStatusCode');
+                }
+            },
+            (error) => {
+                reject(error)
+            }
+        );
+    });
+}
+
+function getTokenUsingAzCLI() {
+    let resultOfExec: IExecSyncResult = execSync("az", "account get-access-token --query \"accessToken\"", { silent: true } as IExecSyncOptions);
+    if (resultOfExec.code != 0) {
+        console.log("##[error]Error Code: [" + resultOfExec.code + "]");
+        core.error("Login with azure/login action to fetch token")
+        throw resultOfExec;
+    }
+    let token = resultOfExec.stdout.trim();
+    return token.substring(1, token.length - 1);
+}
